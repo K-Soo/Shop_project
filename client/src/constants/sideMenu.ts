892cc054b561d7adf4732fed1608661588978d52
Global.Soo <kanabun102@naver.com>
2021-12-20 04:31:56 +0900

    Fix: 사이드메뉴 퀵바 수정

--- client/src/constants/sideMenu.ts
+++ client/src/constants/sideMenu.ts
@@ -3,7 +3,7 @@ export type TQuickIconProps = { [K in TList]: string };
 
 export const QUICK_ICON_LEFT: TQuickIconProps[] = [
   { label: '장바구니', url: '/order/basket', icon: 'cart' },
-  { label: '상품후기', url: '/2', icon: 'bed' },
+  { label: '관심상품', url: '/users/interest-products', icon: 'heart' },
   { label: '공지사항', url: '/board/notice', icon: 'warning' },
   { label: '마이쇼핑', url: '/users', icon: 'user' },
 ]
